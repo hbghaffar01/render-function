870cec2950e21b88773dce1aab5c4f38c5f4c33c
hbghaffar01 <99249242+hbghaffar01@users.noreply.github.com>
2022-06-21 13:20:09 +0500

    render

---
+++ src/components/HeadingView.vue
@@ -0,0 +1,17 @@
+<script>
+import {h} from 'vue'
+export default {
+  props: {
+    level : {
+      default: 1
+    }
+  },
+  setup({level}, {slots}) {
+    const slot = slots.default ? slots.default(): [];
+    const modifeiedSlot = slot.map(({children}) => {
+      return h("div", {style: "color: purple"}, children)
+    })
+    return () => h(`h${level}`, modifeiedSlot)
+  }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
+  <HeadingView>TV Finder</HeadingView>
+  <myShows>
+    <template #sc="{ shows }">
+      <h3 v-if="shows?.length > 0">
+        Average Score {{ computedAverage(shows).toFixed(2) }}
+      </h3>
+    </template>
+  </myShows>
+</template>>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script setup>
+  import HeadingView from '@/components/HeadingView.vue'
+  import myShows from '@/components/myShows.vue';
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  const computedAverage = (shows) =>
+  shows.reduce((acc, info) => +info.score + acc, 0) / shows.length;
 </script>
+
+<style>
+
+</style>
